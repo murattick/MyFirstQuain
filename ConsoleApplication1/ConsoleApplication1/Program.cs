463088a8ec46e18c9685f69354cd0e609a5fdf12
murattick <murattick@gmail.com>
2017-09-13 14:37:28 +0300

    files

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program{
+static void Main(string[] args){
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.CursorVisible = false;
+Console.Clear();
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) {
+string s = @"using System;
+class Program{{
+static void Main(string[] args){{
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.CursorVisible = false;
+Console.Clear();
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) {{
+string s = @{0}{1}{0};
+Console.WriteLine(s,'{0}',s);}}}}}}";
+Console.WriteLine(s, '"', s);}}}
